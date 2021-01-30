60e3c454cae69f3730bf55ce2de2dad8a133f32c
Tiago Brito <tiago.de.oliveira.brito@tecnico.ulisboa.pt>
2021-01-30 18:22:34 +0000

    Added BO loops query and updated run script

---
+++ neo4j-custom-docker/neo4j_queries/loops_bo.cypher
@@ -0,0 +1,31 @@
+// Get loops
+MATCH (f:Function)-[:AST*1..]->(loop:Instruction)
+WHERE loop.instType="Loop"
+
+// Get stores and vars
+WITH *
+MATCH (loop)-[:AST*1..]->(store:Instruction)-[argEdge:AST]->(storeArg:Instruction)-[:AST]->(var:Instruction)
+WHERE store.instType="Store"
+	AND argEdge.arg=0
+    AND storeArg.instType="Binary"
+    AND storeArg.opcode="i32.add"
+    AND (
+    	var.instType="LocalGet"
+        OR var.instType="LocalTee"
+        )
+
+// Check if breaks depend on store vars
+WITH *
+OPTIONAL MATCH path=(loop)-[:AST*1..]->(brIf:Instruction)-[:AST]->(compare:Instruction)-[:AST*1..]->(local:Instruction)
+WHERE brIf.instType="BrIf"
+	AND compare.instType="Compare"
+    AND local.label=var.label
+    AND (
+    	var.instType="LocalGet"
+        OR var.instType="LocalTee"
+        )
+
+WITH path, f, loop
+RETURN DISTINCT
+CASE path WHEN path=null THEN f.name END AS function,
+CASE path WHEN path=null THEN loop.label END AS loop;
--- neo4j-custom-docker/run_neo4j.sh
+++ neo4j-custom-docker/run_neo4j.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DEBUG=false
 
 if [[ -z $1 ]]; then
     echo "$0: Required path containing graph files"
@@ -40,36 +41,49 @@ echo $password | sudo -S chown $UID:$UID -R $NEO4J_QUERIES
 echo "[INFO] - Building image for container $NEO4j_WASMATI_CONTAINER"
 docker build -q . -t neo4j-docker
 
-echo "[INFO] - Running container $NEO4j_WASMATI_CONTAINER"
-docker run -d --rm --name $NEO4J_WASMATI_CONTAINER -v $GRAPH_DIR_PATH:/var/lib/neo4j/import \
-    -v $NEO4J_QUERIES:/var/lib/neo4j/queries \
-    -e NEO4J_apoc_export_file_enabled=true \
-    -e NEO4J_apoc_import_file_enabled=true \
-    -e NEO4J_apoc_import_file_use__neo4j__config=true \
-    -e NEO4JLABS_PLUGINS=\[\"apoc\"\] \
-    -p 7474:7474 -p 7687:7687 neo4j-docker
+if [ "$DEBUG" = true ]; then
+    echo "[INFO] - Running container $NEO4j_WASMATI_CONTAINER"
+    docker run --rm --name $NEO4J_WASMATI_CONTAINER -v $GRAPH_DIR_PATH:/var/lib/neo4j/import \
+        -v $NEO4J_QUERIES:/var/lib/neo4j/queries \
+        -e NEO4J_apoc_export_file_enabled=true \
+        -e NEO4J_apoc_import_file_enabled=true \
+        -e NEO4J_apoc_import_file_use__neo4j__config=true \
+        -e NEO4JLABS_PLUGINS=\[\"apoc\"\] \
+        -p 7474:7474 -p 7687:7687 neo4j-docker
+else
+    echo "[INFO] - Running container $NEO4j_WASMATI_CONTAINER"
+    docker run -d --rm --name $NEO4J_WASMATI_CONTAINER -v $GRAPH_DIR_PATH:/var/lib/neo4j/import \
+        -v $NEO4J_QUERIES:/var/lib/neo4j/queries \
+        -e NEO4J_apoc_export_file_enabled=true \
+        -e NEO4J_apoc_import_file_enabled=true \
+        -e NEO4J_apoc_import_file_use__neo4j__config=true \
+        -e NEO4JLABS_PLUGINS=\[\"apoc\"\] \
+        -p 7474:7474 -p 7687:7687 neo4j-docker
 
-echo "[INFO] - Waiting for server to start..."
-# Check if server already accepting connections
-(echo $password | sudo -S tail -f -n0 `docker inspect --format='{{.LogPath}}' $NEO4J_WASMATI_CONTAINER` &) | grep -q "Started."
-echo "[INFO] - Neo4j server started..."
+    echo "[INFO] - Waiting for server to start..."
+    # Check if server already accepting connections
+    (echo $password | sudo -S tail -f -n0 `docker inspect --format='{{.LogPath}}' $NEO4J_WASMATI_CONTAINER` &) | grep -q "Started."
+    echo "[INFO] - Neo4j server started..."
 
-# RUN queries
-echo "[INFO] - Running queries..."
-docker exec $NEO4J_WASMATI_CONTAINER /var/lib/neo4j/run_queries.sh
+    # RUN queries
+    echo "[INFO] - Running queries..."
+    docker exec $NEO4J_WASMATI_CONTAINER /var/lib/neo4j/run_queries.sh
 
-# Stop container
-echo "[INFO] - Stopping and removing container $NEO4j_WASMATI_CONTAINER"
-docker stop $NEO4J_WASMATI_CONTAINER
+    # Stop container
+    echo "[INFO] - Stopping and removing container $NEO4j_WASMATI_CONTAINER"
+    docker stop $NEO4J_WASMATI_CONTAINER
+fi
 
 # Make sure we have permissions to use graph files (auto chowned by docker...)
 echo $password | sudo -S chown $UID:$UID -R $GRAPH_DIR_PATH
 echo $password | sudo -S chown $UID:$UID -R $NEO4J_QUERIES
 
+if [ "$DEBUG" = false ]; then
 # Move results and times to execution results directory
 mkdir -p $RESULTS_DIR/$GRAPH_DIR_BASE/
 mv $GRAPH_DIR_PATH/*.txt $RESULTS_DIR/$GRAPH_DIR_BASE/
 mv $GRAPH_DIR_PATH/*_result.csv $RESULTS_DIR/$GRAPH_DIR_BASE/
+fi
 
 # Remove dir if was zip
 if [ "$ZIP_FILE" = true ]; then
